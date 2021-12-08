83b27806bb70284a6b3f1f43f9a30b04674e1278
ur4t <46435411+ur4t@users.noreply.github.com>
2021-12-08 18:04:34 +0800

    use a temporary variable in case the captured snippet has side effects

--- src/macros.hpp
+++ src/macros.hpp
@@ -4,15 +4,17 @@
 #ifndef __RUSTY_MACROS_HPP__
 #define __RUSTY_MACROS_HPP__
 
+// use a temporary variable in case that @x contains side effect
 #define dbg(x, ...)                                                            \
     [&] {                                                                      \
+        auto t = (x);                                                          \
         auto flag = ::std::cerr.flags();                                       \
         auto precision = ::std::cerr.precision();                              \
         ::std::cerr << "[" __FILE__ ":" << __LINE__                            \
-                    << "] " #x " = " __VA_ARGS__ << (x) << ::std::endl;        \
+                    << "] " #x " = " __VA_ARGS__ << t << ::std::endl;          \
         ::std::cerr.flags(flag);                                               \
         ::std::cerr.precision(precision);                                      \
-        return x;                                                              \
+        return t;                                                              \
     }()
 
 #define assert_eq(left, right) assert((left) == (right))
